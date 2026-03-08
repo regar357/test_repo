7a1e2d0fdd6b067b9762f65ff3eca43fa597f097
regar357 <regar357@naver.com>
2026-03-08 17:01:02 +0900

    del 123

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD, welcome, 123");
+  res.send("Hello CI/CD, welcome");
 });
 
 app.listen(3000, () => {
