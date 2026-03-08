0c4fe94c0f7abf46bb0cd94d4d5359eb6036122c
regar357 <regar357@naver.com>
2026-03-08 17:43:18 +0900

    add last

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD, welcome, 123");
+  res.send("Hello CI/CD, welcome, 123, last");
 });
 
 app.listen(3000, () => {
